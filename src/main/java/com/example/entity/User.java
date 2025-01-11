4c8b65b8a743a45516ee94fa9e7f5d6393b1d622
Vedant Tathe <tathevedant70@gmail.com>
2025-01-11 22:21:11 +0530

    database changed from localhost to remote

--- src/main/java/com/example/entity/User.java
+++ src/main/java/com/example/entity/User.java
@@ -1,14 +1,14 @@
 package com.example.entity;
 
-
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
 	
 	@Id
