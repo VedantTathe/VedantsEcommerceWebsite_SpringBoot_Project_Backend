a2057b275d31b674978aaa0b957ed787256efda6
Vedant Tathe <tathevedant70@gmail.com>
2025-01-11 20:46:23 +0530

    target folder added

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM maven:3.8.5-openjdk-17 AS build
 WORKDIR /app
 COPY . .
 RUN mvn clean package -DskipTests
+RUN ls -l /app/target/  # This will show the contents of the target directory
 
 # Run stage
 FROM openjdk:17.0.1-jdk-slim
--- src/main/java/com/example/config/WebConfig.java
+++ src/main/java/com/example/config/WebConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 	
-	@Value("${FRONTEND_WEBSITE}")
+	@Value("spring.web.cors.allowed-origin")
     private String websiteUrl;
 	
     @Override
